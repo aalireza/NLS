c3e972fd519f43d2c57fb1bdae4b8b273e78621d
Alireza Rafiei <alireza.rafiei94@gmail.com>
2016-10-01 22:48:31 -0700

    SDHacks ready

--- src/EncodingToolbox.py
+++ src/EncodingToolbox.py
@@ -1,5 +1,6 @@
+from random import choice
+import Data
 import NumericalToolbox
-import MarkovToolbox
 
 # Data from: https://en.wikipedia.org/wiki/Letter_frequency
 FIRST_LETTER_FREQ = {
@@ -10,91 +11,11 @@ FIRST_LETTER_FREQ = {
     "z": 0.034
 }
 
-FREQNT_WORDS = {
-    "a": ["above", "afraid", "against", "agree", "allow", "answer", "apple",
-          "arm", "art", "able", "act", "after", "age", "air", "among",
-          "anger", "arrange", "atom", "about", "add", "again", "ago",
-          "always", "animal", "appear", "area", "arrive", "ask"],
-    "b": ["baby", "ball", "bar", "bat", "beat", "been", "begin", "bell",
-          "blood", "board", "bone", "bot", "box", "bread", "bring",
-          "brother", "build", "buy", "burn", "broad", "break", "boy",
-          "bottom", "book", "boat", "blow", "black", "big", "best", "behind",
-          "before", "beauty", "base", "band", "back", "bad", "bank", "basic",
-          "bear", "bed", "began", "believe", "better", "bird", "block", "body",
-          "born"],
-    "c": ["call", "car", "carry", "catch", "cell", "chair", "character",
-          "check", "child", "claim", "collect", "column", "cool", "corner",
-          "cotton", "cow", "cry", "coat", "compare", "cross", "cold", "color",
-          "common", "complete", "consider", "cook", "cost", "count", "crowd",
-          "cold"],
-    "d": ["dad", "dark", "deal", "decide", "develop", "direct", "divide",
-          "dollar", "door", "draw", "drink", "dance", "discuss", "drive",
-          "danger", "death", "difficult", "dog", "done", "down", "dress"],
-    "e": ["each", "earth", "egg", "electric", "equal", "engine", "example",
-          "exercise", "experiment", "ear", "edge", "element", "equate",
-          "evening", "every", "effect", "energy", "especially", "exact",
-          "excite", "experience"],
-    "f": ["face", "famous", "father", "feet", "fig", "find", "first", "floor",
-          "food", "form", "free", "fruit", "fact", "far", "favor", "fell",
-          "fight", "fine", "fish", "flow", "foot", "forward", "fresh", "full",
-          "fair", "farm", "fear", "felt", "figure", "finger", "fit", "flower",
-          "for", "found", "friend", "fun", "fall", "fast", "feed", "finish",
-          "fly", "family", "front"],
-    "g": ["game", "general", "glad", "good", "gray", "group", "garden",
-          "gentle", "glass", "got", "great", "grow", "gas", "get", "go",
-          "green", "guess", "girl", "gold", "grand"],
-    "h": ["had", "happy", "he", "heat", "here", "history", "hope", "how",
-          "hurry", "hair", "hard", "head", "heavy", "high", "hit", "horse",
-          "huge", "half", "has", "hear", "held", "hill", "hot", "human", "hand",
-          "hat", "heard", "help", "hundred"],
-    "i": ["i", "in", "insect", "iron", "ice", "inch", "instant", "is", "idea",
-          "include", "instrument", "island", "if", "indicate", "interest"],
-    "j": ["job", "join", "joy", "jump", "just"],
-    "k": ["keep", "king", "kept", "key", "knew", "kill", "know", "kind"],
-    "l": ["lady", "last", "lead", "left", "letter", "light", "listen", "lone",
-          "loud", "lake", "late", "learn", "leg", "level", "like", "little",
-          "long", "love", "land", "laugh"],
-    "m": ["machine", "make", "market", "matter", "measure", "metal", "milk",
-          "miss", "money", "most", "mouth", "must", "made", "man", "mass",
-          "may", "meat", "method", "million", "mix", "mix", "month", "mother",
-          "move", "magnet", "many", "Meet"],
-    "n": ["name", "necessary", "new", "noise", "note", "number", "nation",
-          "neck", "next", "noon", "nothing", "numeral", "natual", "need",
-          "night", "notice", "nature", "neighbor", "nine", "north", "near",
-          "now", "never"],
-    "o": ["object", "oil", "only", "order", "out", "observe", "offer", "old",
-          "open", "organ", "over", "occur", "office", "operate", "original",
-          "own", "ocean", "often", "once", "opposite", "other", "one", "our"],
-    "p": ["page", "paper", "paper", "pattern", "phrase", "piece", "planet",
-          "point", "print", "process", "proper", "push", "person", "picture",
-          "pair", "parent", "party", "people", "picture", "place", "plane",
-          "play", "poem", "population", "position", "pound", "pretty",
-          "problem", "product"],
-    "q": ["question", "quick", "quite"],
-    "r": ["race", "rain", "range", "read", "reason", "repeat", "rich", "ring",
-          "road", "room", "rose", "radio", "rather", "receive", "reply",
-          "region", "record", "remember", "represent", "result", "right",
-          "rope", "run"],
-    "s": ["safe", "salt", "sat", "say", "science", "seed", "second", "sentence",
-          "several", "share", "same", "scale", "score", "season", "self",
-          "sense", "settle", "shall", "sharp", "sail", "sand", "saw", "seat",
-          "school", "sea", "see", "segment", "sell", "serve", "seven", "shape",
-          "she", "show", "sing", "skill", "slave", "slow", "son", "star",
-          "story", "subtract", "sudden", "suggest", "sun", "system"],
-    "t": ["table", "talk", "temperature", "thin", "third", "though", "tail",
-          "teeth", "thought", "tire", "told", "took", "track", "trip", "try",
-          "thank", "teach", "there", "thousands", "time", "tool", "trade",
-          "tree"],
-    "u": ["under", "unit", "up", "usual"],
-    "v": ["valley", "village", "value", "vowel", "view", "voice", "visit"],
-    "w": ["want", "water", "week", "where", "what", "while", "window",
-          "wire", "woman", "wonder", "write", "walk", "war", "wash", "wave",
-          "wheel", "wish", "warm", "watch", "weather", "west", "wild", "word"],
-    "y": ["yard", "yes", "young", "year", "yellow", "you", "yet"],
-    "z": ["zebra"]
-}
 
-FILLERS = ["of", "to", "from", "was", "that", "which", "by", "about", "on"]
+def produce_random_freq_filler(letter):
+    phrase = "{} {}".format(choice(Data.FREQUENT_WORDS[letter]),
+                            choice(Data.FILLERS))
+    return phrase
 
 
 def limit_freq_threshold(num):
@@ -120,10 +41,11 @@ def revert_renamed_number(renamed, letters):
     return number
 
 
-def encode(ciphertext, text_model, threshold=10):
-    # Implement Grep
-    freq_limit = limit_freq_threshold(threshold)
-    renamed_ciphertext = first_letter_based_freq_rename(ciphertext, freq_limit)
-    text = MarkovToolbox.generate_text_based_on_letter_list(renamed_ciphertext,
-                                                            text_model)
-    print text
+# def encode(ciphertext, text_model, threshold=10):
+#     # Implement Grep
+#     freq_limit = limit_freq_threshold(threshold)
+#     renamed_ciphertext = first_letter_based_freq_rename(ciphertext, freq_limit)
+#     # text = MarkovToolbox.generate_text_based_on_letter_list(renamed_ciphertext,
+#     #                                                         text_model)
+#     text = 2
+#     print text
--- src/MarkovToolbox.py
+++ src/MarkovToolbox.py
@@ -1,4 +1,5 @@
-from nltk.tokenize import sent_tokenize as split_sentences
+# from nltk.tokenize import sent_tokenize as split_sentences
+import EncodingToolbox
 import cPickle as pickle
 import markovify
 import os
@@ -29,19 +30,63 @@ def load_text_model(model_pickle_abs_path):
 
 
 def make_sentence_starting_with_letter(letter, text_model):
+    sentence = None
     while True:
-        sentence = text_model.make_sentence()
-        if sentence[0] in [letter.lower(), letter.upper()]:
-            return sentence
+        try:
+            sentence = text_model.make_sentence_with_start(
+                EncodingToolbox.produce_random_freq_filler(letter))
+        except KeyError:
+            pass
+        if sentence is not None:
+            if sentence.count('.') == 1:
+                break
+    return sentence
+
+# while True:
+    #     sentence = text_model.make_sentence()
+    #     if sentence[0] in [letter.lower(), letter.upper()]:
+    #         return sentence
 
 
 def generate_text_based_on_letter_list(letters, text_model):
-    text = ' '.join([make_sentence_starting_with_letter(letter, text_model)
-                     for letter in letters])
+    text_list = []
+    for i, letter in enumerate(letters):
+        text_list.append(make_sentence_starting_with_letter(letter, text_model))
+        print "sentence number {} found. Need {} more.".format(
+            i + 1, len(letters) - i - 1)
+    text = ' '.join(text_list)
+    # text = ' '.join([make_sentence_starting_with_letter(letter, text_model)
+    #                  for letter in letters])
     return text
 
 
 def derive_first_letter_of_every_sentence(text):
-    sentences = split_sentences(text)
+    # sentences = split_sentences(text.decode('utf-8'))
+    # To remove examples of P.A.
+    # filtered_text = []
+    # i = 0
+    # while i < len(text) - 1:
+    #     if text[i] == ".":
+    #         if text[i + 1] != " ":
+    #             pass
+    #     filtered_text.append(text[i])
+    #     i += 1
+    # # To remove '.' singletons
+    # extra_filtered_text = []
+    # i = 1
+    # while i < len(filtered_text):
+    #     if filtered_text[i] == ".":
+    #         if filtered_text[i + 1] == " " and filtered_text[i - 1] == " ":
+    #             pass
+    #     extra_filtered_text.append(filtered_text[i])
+    #     i += 1
+    #
+    # text = ''.join(extra_filtered_text)
+
+    sentences = [text[0]] + [text[j]
+                             for j in xrange(2, len(text))
+                             if text[j - 2] == '.' and text[j - 1] == ' ']
+    # sentences = split_sentences(text)
+    # sentences = filter(lambda x: len(x) > 7, text.split('. '))
     letters = [sentence[0].lower() for sentence in sentences]
     return letters
